8faf5548955a76ba1acb64b0e87c22b3b3d3ff2a
tianchoy <tianchoy@live.com>
2024-01-22 17:21:59 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace yankong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(19, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "波特率";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(112, 178);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 34);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(35, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "速度";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton1.Location = new System.Drawing.Point(372, 175);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(89, 32);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "正转";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.radioButton2.Location = new System.Drawing.Point(504, 175);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(89, 32);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "反转";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(24, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 60);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "启动电机";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(241, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 60);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(18, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 25);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "串口号";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(112, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(173, 35);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "19200",
+            "38400",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(112, 100);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(173, 35);
+            this.comboBox2.TabIndex = 11;
+            this.comboBox2.Text = "9600";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(384, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(209, 64);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "打开串口";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.Location = new System.Drawing.Point(384, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(209, 64);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "关闭串口";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 358);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "严控电机测试工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace yankong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+        }
+        private void Updata_Serialport_Name(ComboBox MycomboBox)
+        {
+            string[] ArryPort;                               // 定义字符串数组，数组名为 ArryPort
+            ArryPort = SerialPort.GetPortNames();            // SerialPort.GetPortNames()函数功能为获取计算机所有可用串口，以字符串数组形式输出
+            MycomboBox.Items.Clear();                        // 清除当前组合框下拉菜单内容                  
+            for (int i = 0; i < ArryPort.Length; i++)
+            {
+                MycomboBox.Items.Add(ArryPort[i]);           // 将所有的可用串口号添加到端口对应的组合框中
+            }
+        }
+        private bool SetSpeed(int speed, int waitTime = 200)
+        {
+            try
+            {
+                var speedBytes = BitConverter.GetBytes(speed);
+                var bytes = new byte[8];
+                bytes[0] = 0x01;
+                bytes[1] = 0x06; //功能码
+                bytes[2] = 0x00; //寄存器地址
+                bytes[3] = 0x23; //寄存器地址
+                bytes[4] = speedBytes[1]; //写入数据
+                bytes[5] = speedBytes[0]; //写入数据
+
+                var tmpData = new List<byte>();
+                for (int i = 0; i <= 5; i++)
+                {
+                    tmpData.Add(bytes[i]);
+                }
+                byte dl, dh;
+                CalCRC(tmpData.ToArray(), out dl, out dh);
+                bytes[6] = dl;//1-6字节累加校正码，低
+                bytes[7] = dh;//1-6字节累加校正码，高
+
+                for (var i = 0; i < 200; i++)
+                {
+                    serialPort1.Write(bytes, 0, bytes.Length);
+                    Thread.Sleep(waitTime);
+                    for (var j = 0; j < 200; j++)
+                    {
+                        var backData = new byte[serialPort1.BytesToRead]; //读取
+                        if (serialPort1.Read(backData, 0, backData.Length) < 1) continue;
+                        if (backData.Length < 8) continue;
+
+                        bool flag = true;
+                        for (int k = 0; k < 8; k++)
+                        {
+                            if (backData[k] != bytes[k])
+                            {
+                                flag = false;
+                            }
+                        }
+                        if (!flag) continue;
+
+                        //发出和返回完全一致，则通过
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public bool Start(int speed, bool fwd)
+        {
+            string way = fwd ? "正向" : "反向";
+
+            speed = fwd ? speed : -speed; //速度正数正向，负数负向
+            if (!SetSpeed(speed)) return false;
+
+            try
+            {
+                var bytes = new byte[8];
+                bytes[0] = 0x01; //地址
+                bytes[1] = 0x06; //功能码
+                bytes[2] = 0x00; //寄存器地址
+                bytes[3] = 0x27; //寄存器地址
+                bytes[4] = 0x00; //数据
+                bytes[5] = 0x02; //数据  //速度模式
+
+                var tmpData = new List<byte>();
+                for (int i = 0; i <= 5; i++)
+                {
+                    tmpData.Add(bytes[i]);
+                }
+                byte dl, dh;
+                CalCRC(tmpData.ToArray(), out dl, out dh);
+                bytes[6] = dl;//1-6字节累加校正码，低
+                bytes[7] = dh;//1-6字节累加校正码，高
+
+                
+                for (var i = 0; i < 200; i++)
+                {
+                    serialPort1.Write(bytes, 0, bytes.Length);
+                    Thread.Sleep(200);
+                    for (var j = 0; j < 200; j++)
+                    {
+                        var backData = new byte[serialPort1.BytesToRead]; //读取
+                        if (serialPort1.Read(backData, 0, backData.Length) < 1) continue;
+                        if (backData.Length < 8) continue;
+
+                        bool flag = true;
+                        for (int k = 0; k < 8; k++)
+                        {
+                            if (backData[k] != bytes[k])
+                            {
+                                flag = false;
+                            }
+                        }
+                        if (!flag) continue;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        //启动电机
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool fx = true;
+            if (radioButton1.Checked)
+            {
+                fx = true;
+            }
+            else
+            {
+                fx = false;
+            }
+            Start(int.Parse(textBox1.Text),fx);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Updata_Serialport_Name(comboBox1); // 调用更新可用串口函数，comboBox1为端口号组合框的名称
+        }
+
+        //打开串口
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try                                                          // try 是尝试部分，如果尝试过程中出现问题，进入catch部分，执行错误处理代码  
+            {
+                serialPort1.PortName = comboBox1.Text;                   // 将串口设备的串口号属性设置为comboBox1复选框中选择的串口号
+                serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);  // 将串口设备的波特率属性设置为comboBox2复选框中选择的波特率
+                serialPort1.Open();                                      // 打开串口
+                comboBox1.Enabled = false;                               // 串口已打开，将comboBox1设置为不可操作
+                comboBox2.Enabled = false;                               // 串口已打开，将comboBox2设置为不可操作
+                button3.Enabled = false;
+                button4.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("打开串口失败，请检查串口", "错误");     // 弹出错误对话框
+            }
+        }
+
+        //关闭串口
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();                     // 关闭串口
+                comboBox1.Enabled = true;                // 串口已关闭，将comboBox1设置为可操作
+                comboBox2.Enabled = true;                // 串口已关闭，将comboBox2设置为可操作
+                button3.Enabled = true;
+                button4.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("关闭串口失败，请检查串口", "错误");   // 弹出错误对话框
+            }
+        }
+
+        //关闭电机
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var bytes = new byte[8];
+                bytes[0] = 0x01;
+                bytes[1] = 0x06; //功能码
+                bytes[2] = 0x00; //寄存器地址
+                bytes[3] = 0x28; //寄存器地址
+                bytes[4] = 0x00; //数据
+                bytes[5] = 0x00; //数据
+
+                var tmpData = new List<byte>();
+                for (int i = 0; i <= 5; i++)
+                {
+                    tmpData.Add(bytes[i]);
+                }
+                byte dl, dh;
+                CalCRC(tmpData.ToArray(), out dl, out dh);
+                bytes[6] = dl;//1-6字节累加校正码，低
+                bytes[7] = dh;//1-6字节累加校正码，高
+
+                serialPort1.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        public static void CalCRC(byte[] bytes, out byte dl, out byte dh)
+        {
+            int crc = 0xffff;
+            int len = bytes.Length;
+            for (int n = 0; n < len; n++)
+            {
+                byte i;
+                crc = crc ^ bytes[n];
+                for (i = 0; i < 8; i++)
+                {
+                    int TT;
+                    TT = crc & 1;
+                    crc = crc >> 1;
+                    crc = crc & 0x7fff;
+                    if (TT == 1)
+                    {
+                        crc = crc ^ 0xa001;
+                    }
+                    crc = crc & 0xffff;
+                }
+
+            }
+            dl = (byte)((crc & 0xff));
+            dh = (byte)((crc >> 8) & 0xff);
+        }
+    }
+}
